4ea092aa9ff82467f1b242bbc08baa9daf4f8516
Alexandre Archambault <alexarchambault@users.noreply.github.com>
2023-05-11 21:13:12 +0200

    Substitute properties before splitting input class paths (#4)

--- coursier/cputil/ClassPathUtil.scala
+++ coursier/cputil/ClassPathUtil.scala
@@ -24,45 +24,49 @@ object ClassPathUtil {
   def classPath(input: String): Seq[Path] =
     classPath(input, sys.props.get)
 
+  def substituteProperties(input: String, getProperty: String => Option[String]): String = {
+    var value            = input
+    var matcher: Matcher = null
+
+    while ({
+      matcher = propertyRegex.matcher(value)
+      matcher.find()
+    }) {
+      val start    = matcher.start(0)
+      val end      = matcher.end(0)
+      val subKey   = value.substring(start + 2, end - 1)
+      val subValue = getProperty(subKey).getOrElse("")
+      value = value.substring(0, start) + subValue + value.substring(end)
+    }
+
+    value
+  }
+
+  private def allJarsOf(dir: Path): Seq[Path] =
+    if (Files.isDirectory(dir))
+      Files.list(dir)
+        .iterator
+        .asScala
+        .filter(_.toString.endsWithIgnoreCase(".jar"))
+        .toVector
+        .sortBy(_.getFileName)
+    else
+      Nil
+
+  private val allJarsSuffixes = Seq("/", File.separator)
+    .distinct
+    .flatMap(sep => Seq("*", "*.jar").map(sep + _))
+
   def classPath(input: String, getProperty: String => Option[String]): Seq[Path] =
-    input.split(File.pathSeparator).filter(_.nonEmpty).flatMap { elem =>
-      val processedElem = {
-        var value            = elem
-        var matcher: Matcher = null
-
-        while ({
-          matcher = propertyRegex.matcher(value)
-          matcher.find()
-        }) {
-          val start    = matcher.start(0)
-          val end      = matcher.end(0)
-          val subKey   = value.substring(start + 2, end - 1)
-          val subValue = getProperty(subKey).getOrElse("")
-          value = value.substring(0, start) + subValue + value.substring(end)
+    substituteProperties(input, getProperty).split(File.pathSeparator).filter(_.nonEmpty).flatMap {
+      elem =>
+        allJarsSuffixes.find(elem.endsWithIgnoreCase) match {
+          case Some(suffix) =>
+            val dir = Paths.get(elem.substring(0, elem.length - suffix.length))
+            allJarsOf(dir)
+          case None =>
+            Seq(Paths.get(elem))
         }
-
-        value
-      }
-      def allJarsOf(dir: Path): Seq[Path] =
-        if (Files.isDirectory(dir))
-          Files.list(dir)
-            .iterator
-            .asScala
-            .filter(_.toString.endsWithIgnoreCase(".jar"))
-            .toVector
-            .sortBy(_.getFileName)
-        else
-          Nil
-      val allJarsSuffixes = Seq("/", File.separator)
-        .distinct
-        .flatMap(sep => Seq("*", "*.jar").map(sep + _))
-      allJarsSuffixes.find(processedElem.endsWithIgnoreCase) match {
-        case Some(suffix) =>
-          val dir = Paths.get(processedElem.substring(0, processedElem.length - suffix.length))
-          allJarsOf(dir)
-        case None =>
-          Seq(Paths.get(processedElem))
-      }
     }
 
 }
